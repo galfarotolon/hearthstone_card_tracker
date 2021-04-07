54870b4853c24163e9641977d82c123a2ddbe627
Guillermo Alfaro <galfarotolon@gmail.com>
2021-04-06 22:17:59 -0500

    map through curr expansion

--- src/components/Home.js
+++ src/components/Home.js
@@ -47,7 +47,7 @@ const Home = () => {
 
     const allCardImages = []
 
-
+    //check the current expansion, if its not empty, map through it
     if (fitb) {
         fitb.map(cards => {
             // console.log(cards)
